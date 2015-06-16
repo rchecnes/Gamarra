6cf9230b2b735e657d33c24805aedb0825550856
rchecnes <richardchecnes@hotmail.com>
2015-06-16 18:30:52 -0500

    subida de fotos

---
+++ src/Crece/RegistroBundle/Controller/DocumentController.php
@@ -0,0 +1,234 @@
+<?php
+
+namespace Crece\RegistroBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Crece\RegistroBundle\Entity\Document;
+use Crece\RegistroBundle\Form\DocumentType;
+
+/**
+ * Document controller.
+ *
+ */
+class DocumentController extends Controller
+{
+
+    /**
+     * Lists all Document entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+
+        $entities = $em->getRepository('CreceRegistroBundle:Document')->findAll();
+
+        $data['titulo'] = "Documentos";
+        $data['entities'] = $entities;
+
+        return $this->render('CreceRegistroBundle:Document:index.html.twig', $data);
+    }
+    /**
+     * Creates a new Document entity.
+     *
+     */
+    public function createAction(Request $request)
+    {
+        
+        $entity = new Document();
+        $form = $this->createForm(new DocumentType(), $entity);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity->upload();
+            $em->persist($entity);
+            $em->flush();
+
+            
+
+            return $this->redirect($this->generateUrl('document', array()));
+        }
+
+        $data['titulo'] = "Documentos";
+        $data['entity'] = $entity;
+        $data['form']   = $form->createView();
+
+        return $this->render('CreceRegistroBundle:Document:new.html.twig', $data);
+    }
+
+    /**
+     * Creates a form to create a Document entity.
+     *
+     * @param Document $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCreateForm(Document $entity)
+    {
+        $form = $this->createForm(new DocumentType(), $entity, array(
+            'action' => $this->generateUrl('document_create'),
+            'method' => 'POST',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Create'));
+
+        return $form;
+    }
+
+    /**
+     * Displays a form to create a new Document entity.
+     *
+     */
+    public function newAction()
+    {
+        $entity = new Document();
+        $form = $this->createForm(new DocumentType(), $entity);
+        //$form   = $this->createCreateForm($entity);
+
+        $data['titulo'] = "Documentos";
+        $data['entity'] = $entity;
+        $data['form'] = $form->createView();
+
+        return $this->render('CreceRegistroBundle:Document:new.html.twig', $data);
+    }
+
+    /**
+     * Finds and displays a Document entity.
+     *
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('CreceRegistroBundle:Document')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Document entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('CreceRegistroBundle:Document:show.html.twig', array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Document entity.
+     *
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('CreceRegistroBundle:Document')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Document entity.');
+        }
+
+        $editForm = $this->createEditForm($entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('CreceRegistroBundle:Document:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+    * Creates a form to edit a Document entity.
+    *
+    * @param Document $entity The entity
+    *
+    * @return \Symfony\Component\Form\Form The form
+    */
+    private function createEditForm(Document $entity)
+    {
+        $form = $this->createForm(new DocumentType(), $entity, array(
+            'action' => $this->generateUrl('document_update', array('id' => $entity->getId())),
+            'method' => 'PUT',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Update'));
+
+        return $form;
+    }
+    /**
+     * Edits an existing Document entity.
+     *
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('CreceRegistroBundle:Document')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Document entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createEditForm($entity);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isValid()) {
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('document_edit', array('id' => $id)));
+        }
+
+        return $this->render('CreceRegistroBundle:Document:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+    /**
+     * Deletes a Document entity.
+     *
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('CreceRegistroBundle:Document')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Document entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('document'));
+    }
+
+    /**
+     * Creates a form to delete a Document entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('document_delete', array('id' => $id)))
+            ->setMethod('DELETE')
+            ->add('submit', 'submit', array('label' => 'Delete'))
+            ->getForm()
+        ;
+    }
+}
---
+++ src/Crece/RegistroBundle/Form/DocumentType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Crece\RegistroBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class DocumentType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('file')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Crece\RegistroBundle\Entity\Document'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'crece_registrobundle_document';
+    }
+}
